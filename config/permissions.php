302f471dfe56c7822c5cae5d46ee7a42fb2d7e56
Zoltan Hunt <zhunt@zee4.com>
2016-07-30 00:54:31 -0400

    cakeDC user update

---
+++ config/permissions.php
@@ -0,0 +1,12 @@
+<?php
+return [
+        'Users.SimpleRbac.permissions' => [
+            [
+                'role' => 'user',
+                'controller' => 'Posts',
+                'action' => ['view'],
+            ],
+        ]
+    ];
+
+?>
